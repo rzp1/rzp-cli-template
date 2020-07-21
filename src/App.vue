39fe7e0c3e44ec2ff83af71ace14d034b5776f5c
任志鹏 <rzp@meitu.com>
2020-07-21 10:32:27 +0800

    feat

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    {{#if langChange}}
+    <router-view/>
+    {{else}}
+    <HelloWorld/>
+    {{/if}}
+  </div>
+</template>
+
+<script>
+{{#unless langChange}}
+import HelloWorld from './components/HelloWorld'
+
+{{/unless}}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
